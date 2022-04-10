e3097ed67dc56be737fe564266a8e6cd285888a0
Mayya Markova <mayya.markova@gmail.com>
2022-04-10 14:47:10 +0300

    Add Article model by using Gii

---
+++ models/Article.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "article".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $slug
+ * @property string $body
+ * @property int $created_at
+ * @property int $created_by
+ * @property int $updated_at
+ *
+ * @property User $createdBy
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'slug', 'body', 'created_at', 'created_by', 'updated_at'], 'required'],
+            [['body'], 'string'],
+            [['created_at', 'created_by', 'updated_at'], 'integer'],
+            [['title', 'slug'], 'string', 'max' => 1024],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['created_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'slug' => 'Slug',
+            'body' => 'Body',
+            'created_at' => 'Created At',
+            'created_by' => 'Created By',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::className(), ['id' => 'created_by']);
+    }
+}
